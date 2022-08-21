6757975e705575fe52e718aa9eb20258c6b45448
Mehmet <mathdenizi@gmail.com>
2022-08-21 18:47:57 +0200

    db config is ready

---
+++ Spring-02-ORM/src/main/java/com/zeroToHero/Spring02OrmApplication.java
@@ -0,0 +1,13 @@
+package com.zeroToHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring02OrmApplication.class, args);
+    }
+
+}
